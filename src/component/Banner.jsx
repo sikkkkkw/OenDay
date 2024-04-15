c026d5d9f52da3abd62b7561cdd5218022352bd1
sikkkkkw <gksmsla92@naver.com>
2024-04-15 15:16:20 +0900

    test

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import bell from "../img/bannerBell.png";
 import useUser from "./useUser";
 
-
 export default function Banner() {
   const userData = useUser();
   const userName = userData;
