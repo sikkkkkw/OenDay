4d468276ec49500dc78919ecda4223694321388a
sikkkkkw <gksmsla92@naver.com>
2024-04-15 15:37:30 +0900

    제발

--- src/component/New.jsx
+++ src/component/New.jsx
@@ -72,4 +72,5 @@ export default function Popular({ dark }) {
       {showModal && <Modal dark={dark} modalRef={modalRef} imageUrl={selectedImage} onClose={closeModal} content={modalCont} />}
     </>
   );
+  
 }
