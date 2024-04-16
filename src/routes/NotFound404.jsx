faf2bfd3c12d8aaa57d6c80dd3f0527cf0acb745
parkjunhyun <jhyun970405@gmail.com>
2024-04-16 13:53:29 +0900

    egg

--- src/routes/NotFound404.jsx
+++ src/routes/NotFound404.jsx
@@ -21,7 +21,7 @@ export default function NotFound404() {
       <Link to="/">
         <img src={logo} alt="하루만 로고" className="absolute top-4 left-[20px] w-[114px]" />
       </Link>
-      <div className="w-full max-w-[80%] h-screen flex flex-col justify-center items-center">
+      <div className="w-full max-w-[80%] h-screen flex flex-col justify-center items-center pt-[45px]">
         <img src={notf} alt="404 not found" className="w-full max-w-[700px]" />
         <p className="font-bold text-[34px] mt-8 mb-6">페이지를 찾을 수 없습니다.</p>
         <button>
