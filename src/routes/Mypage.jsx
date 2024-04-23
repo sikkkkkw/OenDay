3cb1a287f974078fcd9205102016ae07298bfa22
ldh <ai04120@naver.com>
2024-04-23 10:43:43 +0900

    1

--- src/routes/Mypage.jsx
+++ src/routes/Mypage.jsx
@@ -29,7 +29,7 @@ export default function Mypage() {
     e.stopPropagation();
   };
 
-  // // 세션 스토리지에서 데이터 가져오기
+  // 세션 스토리지에서 데이터 가져오기
   const storedData = JSON.parse(sessionStorage.getItem("DataArray"));
   return (
     <div className="w-full flex flex-col items-center p-4 gap-y-7">
